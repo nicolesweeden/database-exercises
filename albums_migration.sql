bb9b5d1a303637adc16df087ace0ab2b1757334d
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-01-23 15:41:47 -0700

    finished SQL Tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE albums (
+                id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                artist VARCHAR(50),
+                name VARCHAR(100),
+                release_date int,
+                sales float,
+                genre VARCHAR(50),
+                PRIMARY KEY (id)
+);
