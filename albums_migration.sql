06a8b50e52bdcc45a57154c65a098aa2dbe3dcc9
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-03 22:29:48 -0700

    finished sql tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,12 @@
 USE codeup_test_db;
-
+DROP TABLE albums;
 CREATE TABLE albums (
                 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                 artist VARCHAR(50),
                 name VARCHAR(100),
                 release_date int,
                 sales float,
+                random int,
                 genre VARCHAR(50),
                 PRIMARY KEY (id)
 );
@@ -18,13 +19,3 @@ CREATE TABLE albums (
 # # release_date — integer representing year record was released
 # # sales — floating point value for number of records sold (in millions)
 # # genre — string for storing the record's genre(s)
-#
-# CREATE TABLE albums (
-#   id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-#   artist VARCHAR(50) NOT NULL,
-#   name VARCHAR(100) NOT NULL,
-#   release_date INT NOT NULL,
-#   sales FLOAT NOT NULL,
-#   genre VARCHAR (50) NOT NULL,
-#   PRIMARY KEY (id)
-# );
