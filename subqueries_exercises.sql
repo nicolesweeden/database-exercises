cd9b7bd3f200cf097f493e3a772d7cade3c4d82b
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-27 15:59:08 -0700

    exercise

---
+++ subqueries_exercises.sql
@@ -0,0 +1,10 @@
+# Find all the employees with the same hire date as employee 101010 using a subquery.
+#
+# 69 Rows
+#
+# Find all the titles held by all employees with the first name Aamod.
+#
+# 314 total titles, 6 unique titles
+#
+# Find all the current department managers that are female.
+
